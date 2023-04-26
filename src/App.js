2a43061bdc31e423d6ebb174647b6d9d592bcca5
n07Laksh <112006645+n07Laksh@users.noreply.github.com>
2023-04-26 21:00:51 +0530

    added alert component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+import { useState } from 'react'
 import './App.css';
 import {
   BrowserRouter as R,
@@ -10,19 +10,38 @@ import Home from './component/Home';
 import About from './component/About';
 import NoteState from './context/Notes/notesState';
 import 'react-confirm-alert/src/react-confirm-alert.css'; // Import css
+import Login from './component/Login';
+import SignUp from './component/SignUp';
+import Alert from './component/Alert';
+
 
 
 function App() {
+  const [alert, setAlert] = useState(null);
+  const showAlert = (message, type) => {
+    setAlert({
+      message: message,
+      type: type
+    })
+    setTimeout(() => setAlert(null), 3000)
+  }
   return (
     <>
-    <NoteState>
-      <R>
-        <Navabar />
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/about" element={<About />} />
-        </Routes>
-      </R>
+      <NoteState>
+        <R>
+          <Navabar />
+
+          <div style={{ height: "50px", zIndex: 1,width: "100%"}} className='position-fixed'>
+            <Alert alert={alert} />
+          </div>
+
+          <Routes>
+            <Route exact path="/" element={<Home showAlert={showAlert} />} />
+            <Route exact path="/about" element={<About showAlert={showAlert} />} />
+            <Route exact path="/login" element={<Login showAlert={showAlert} />} />
+            <Route exact path="/signup" element={<SignUp showAlert={showAlert} />} />
+          </Routes>
+        </R>
       </NoteState>
     </>
   );
